17fe5987d6e5cf73ea9723d1d1e780c5e61a836b
coderlei <972761675@qq.com>
2022-05-30 11:10:03 +0800

    upload files stu...

--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,11 +1,10 @@
 <script setup lang='ts'>
-import TodoList from "@/components/todo-list.vue"
 </script>
+
 <template>
-  <el-card>
-    <TodoList/>
-  </el-card>
+
 </template>
 
 <style lang='scss' scoped>
+
 </style>
