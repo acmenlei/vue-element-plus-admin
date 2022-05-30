a19790e27a3f0393cf2bf7256fb79d1b3d923dc5
coderlei <972761675@qq.com>
2022-05-30 15:56:24 +0800

    fix: 代码格式优化

--- src/service/todo.ts
+++ src/service/todo.ts
@@ -1,9 +1,8 @@
+const REQUEST_URL = "https://getpantry.cloud/apiv1/pantry/50d468e6-7cc6-4960-839d-3f5ebf478688/basket/coderlei_todo"
+
 export function query() {
   return new Promise((resolve) => {
-    fetch("https://getpantry.cloud/apiv1/pantry/50d468e6-7cc6-4960-839d-3f5ebf478688/basket/coderlei_todo")
-      .then(resp => resp.json().then(rest => {
-        resolve(rest)
-      }))
+    fetch(REQUEST_URL).then(resp => resp.json().then(rest => resolve(rest)))
   })
 }
 
@@ -11,10 +10,8 @@ export function update(data: any) {
   return new Promise((resolve) => {
     let headers = new Headers()
     headers.append("Content-Type", "application/json")
-    fetch("https://getpantry.cloud/apiv1/pantry/50d468e6-7cc6-4960-839d-3f5ebf478688/basket/coderlei_todo", { method: "POST", body: JSON.stringify(data), headers })
-      .then(resp => resp.json().then(rest => {
-        resolve(rest)
-      }))
+    fetch(REQUEST_URL, { method: "POST", body: JSON.stringify(data), headers })
+      .then(resp => resp.json().then(rest => resolve(rest)))
   })
 }
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,10 +2,8 @@
 import TodoList from "@/components/todo/todo-list.vue"
 </script>
 <template>
-  <el-card>
     <TodoList/>
-  </el-card>
 </template>
 
-<style lang='scss' scoped>
+<style scoped>
 </style>
